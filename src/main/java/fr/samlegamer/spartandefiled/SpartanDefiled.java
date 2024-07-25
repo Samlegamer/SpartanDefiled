f8d559483d9c8f70b84e85b508789e3ff04402be
Samlegamer <samlegamer.yt@gmail.com>
2024-07-26 00:58:29 +0200

    change version

--- src/main/java/fr/samlegamer/spartandefiled/SpartanDefiled.java
+++ src/main/java/fr/samlegamer/spartandefiled/SpartanDefiled.java
@@ -2,7 +2,7 @@ package fr.samlegamer.spartandefiled;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = SpartanDefiled.MODID, name = "Spartan Defiled", version = "1.12.2-1.1")
+@Mod(modid = SpartanDefiled.MODID, name = "Spartan Defiled", version = "1.12.2-1.2")
 public class SpartanDefiled
 {
     public static final String MODID = "spartandefiled";	
